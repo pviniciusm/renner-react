4d3b178cec5250cd2fe052a30932a5e5f56a545c
Paulo <pvinics@gmail.com>
2024-02-15 15:40:34 -0300

    card de produto

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { CssBaseline } from "@mui/material";
 import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 
 function App() {
     return (
         <>
             <CssBaseline />
             <Header />
-            <div>
-                <h1>Teste</h1>
-            </div>
+            <Home />
         </>
     );
 }
