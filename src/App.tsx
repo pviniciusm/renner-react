e1adca68d323eeb49f7b73b227daba6b9b87e365
Paulo <pvinics@gmail.com>
2024-02-15 16:07:43 -0300

    rudux config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { CssBaseline } from "@mui/material";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
+import { Provider } from "react-redux";
+import { store } from "./config/store";
 
 function App() {
     return (
         <>
-            <CssBaseline />
-            <Header />
-            <Home />
+            <Provider store={store}>
+                <CssBaseline />
+                <Header />
+                <Home />
+            </Provider>
         </>
     );
 }
